84559c1715797194f4937a576a5548b1a498f288
larry <xspanni@gmail.com>
2021-07-10 21:56:09 +0800

    在官方示例基础上修改，尝试解决光标消失问题

--- projects/some_web/src/js/views/example/richtext.tsx
+++ projects/some_web/src/js/views/example/richtext.tsx
@@ -1,16 +1,19 @@
 import React, { useCallback, useMemo, useState } from 'react'
 import isHotkey from 'is-hotkey'
-import { Editable, withReact, useSlate, Slate } from 'slate-react'
+import {Editable, withReact, useSlate, Slate, ReactEditor} from 'slate-react'
 import {
     Editor,
     Transforms,
     createEditor,
     Descendant,
-    Element as SlateElement,
+    Element as SlateElement, Node as SlateNode, Path as SlatePath,
 } from 'slate'
 import { withHistory } from 'slate-history'
 
 import { Button, Icon, Toolbar } from './components'
+import {SFHeaderNode, SFHeaderView} from "@/js/components/editor/nodes/header";
+import {SFTextNode, SFTextView} from "@/js/components/editor/nodes/text";
+import {SFParagraphNode} from "@/js/components/editor/nodes/paragraph";
 
 const HOTKEYS = {
     'mod+b': 'bold',
@@ -30,15 +33,8 @@ const RichTextExample = () => {
     return (
         <Slate editor={editor} value={value} onChange={value => setValue(value)}>
             <Toolbar>
-                <MarkButton format="bold" icon="format_bold" />
-                <MarkButton format="italic" icon="format_italic" />
-                <MarkButton format="underline" icon="format_underlined" />
-                <MarkButton format="code" icon="code" />
                 <BlockButton format="heading-one" icon="looks_one" />
                 <BlockButton format="heading-two" icon="looks_two" />
-                <BlockButton format="block-quote" icon="format_quote" />
-                <BlockButton format="numbered-list" icon="format_list_numbered" />
-                <BlockButton format="bulleted-list" icon="format_list_bulleted" />
             </Toolbar>
             <Editable
                 renderElement={renderElement}
@@ -46,128 +42,59 @@ const RichTextExample = () => {
                 placeholder="Enter some rich text…"
                 spellCheck
                 autoFocus
-                onKeyDown={event => {
-                    for (const hotkey in HOTKEYS) {
-                        if (isHotkey(hotkey, event as any)) {
-                            event.preventDefault()
-                            const mark = HOTKEYS[hotkey]
-                            toggleMark(editor, mark)
-                        }
-                    }
-                }}
             />
         </Slate>
     )
 }
 
-const toggleBlock = (editor, format) => {
-    const isActive = isBlockActive(editor, format)
-    const isList = LIST_TYPES.includes(format)
-
-    Transforms.unwrapNodes(editor, {
-        match: n =>
-            LIST_TYPES.includes(
-                !Editor.isEditor(n) && SlateElement.isElement(n) && n.type
-            ),
-        split: true,
-    })
-    const newProperties: Partial<SlateElement> = {
-        type: isActive ? 'paragraph' : isList ? 'list-item' : format,
+function toggleBlock(editor: ReactEditor, node: {name:string}) {
+    console.debug("toggleBlock", node)
+    let props: any = node;
+    if (isBlockActive(editor, node)) {
+        props = {name:"paragraph"};
     }
-    Transforms.setNodes(editor, newProperties)
-
-    if (!isActive && isList) {
-        const block = { type: format, children: [] }
-        Transforms.wrapNodes(editor, block)
-    }
+    Transforms.setNodes(editor, props);
 }
 
-const toggleMark = (editor, format) => {
-    const isActive = isMarkActive(editor, format)
-
-    if (isActive) {
-        Editor.removeMark(editor, format)
-    } else {
-        Editor.addMark(editor, format, true)
-    }
-}
-
-const isBlockActive = (editor, format) => {
+function isBlockActive(editor: ReactEditor, headerNode: { name:string }): boolean {
+    console.debug("isBlockActive", headerNode)
     const [match] = Editor.nodes(editor, {
-        match: n =>
-            !Editor.isEditor(n) && SlateElement.isElement(n) && n.type === format,
+        match: (n: string, p: SlatePath) => {
+            console.debug("isBlockActive====", n)
+            return !Editor.isEditor(n) && SlateElement.isElement(n) &&
+                n.name === headerNode.name ;
+        },
     })
-
     return !!match
 }
 
-const isMarkActive = (editor, format) => {
-    const marks = Editor.marks(editor)
-    return marks ? marks[format] === true : false
+
+function Element({ attributes, children, element }:{attributes: any, children: any, element: any}) {
+    console.debug("renderElement", element, attributes, children);
+    if (element.name === "heading-one") {
+        //return <SFHeaderView attributes={attributes} children={children} node={element as SFHeaderNode} />
+        return <h1 {...attributes} >{children}</h1>
+    }
+    return <span style={{marginTop:0, marginBottom:0, minHeight:16 }} {...attributes}>{children}</span>
 }
 
-const Element = ({ attributes, children, element }) => {
-    switch (element.type) {
-        case 'block-quote':
-            return <blockquote {...attributes}>{children}</blockquote>
-        case 'bulleted-list':
-            return <ul {...attributes}>{children}</ul>
-        case 'heading-one':
-            return <h1 {...attributes}>{children}</h1>
-        case 'heading-two':
-            return <h2 {...attributes}>{children}</h2>
-        case 'list-item':
-            return <li {...attributes}>{children}</li>
-        case 'numbered-list':
-            return <ol {...attributes}>{children}</ol>
-        default:
-            return <p {...attributes}>{children}</p>
+function Leaf({ attributes, children, leaf }:{attributes: any, children: any, leaf: any}) {
+    console.debug("renderLeaf", leaf, attributes, children);
+    if (leaf.name === "text") {
+        return <SFTextView attributes={attributes} children={children} node={leaf as SFTextNode}/>
     }
-}
-
-const Leaf = ({ attributes, children, leaf }) => {
-    if (leaf.bold) {
-        children = <strong>{children}</strong>
-    }
-
-    if (leaf.code) {
-        children = <code>{children}</code>
-    }
-
-    if (leaf.italic) {
-        children = <em>{children}</em>
-    }
-
-    if (leaf.underline) {
-        children = <u>{children}</u>
-    }
-
     return <span {...attributes}>{children}</span>
 }
+
 
 const BlockButton = ({ format, icon }) => {
     const editor = useSlate()
     return (
         <Button
-            active={isBlockActive(editor, format)}
+            active={isBlockActive(editor, {name:format})}
             onMouseDown={event => {
                 event.preventDefault()
-                toggleBlock(editor, format)
-            }}
-        >
-            <Icon>{icon}</Icon>
-        </Button>
-    )
-}
-
-const MarkButton = ({ format, icon }) => {
-    const editor = useSlate()
-    return (
-        <Button
-            active={isMarkActive(editor, format)}
-            onMouseDown={event => {
-                event.preventDefault()
-                toggleMark(editor, format)
+                toggleBlock(editor, {name:format})
             }}
         >
             <Icon>{icon}</Icon>
@@ -177,37 +104,16 @@ const MarkButton = ({ format, icon }) => {
 
 const initialValue: Descendant[] = [
     {
-        type: 'paragraph',
-        children: [
-            { text: 'This is editable ' },
-            { text: 'rich', bold: true },
-            { text: ' text, ' },
-            { text: 'much', italic: true },
-            { text: ' better than a ' },
-            { text: '<textarea>', code: true },
-            { text: '!' },
-        ],
-    },
-    {
-        type: 'paragraph',
+        name: 'paragraph',
         children: [
             {
                 text:
                     "Since it's rich text, you can do things like turn a selection of text ",
             },
-            { text: 'bold', bold: true },
-            {
-                text:
-                    ', or add a semantically rendered block quote in the middle of the page, like this:',
-            },
         ],
     },
     {
-        type: 'block-quote',
-        children: [{ text: 'A wise quote.' }],
-    },
-    {
-        type: 'paragraph',
+        name: 'paragraph',
         children: [{ text: 'Try it out for yourself!' }],
     },
 ]
