397865731fd093e8ebc1b72395082df1727eaf94
larry <xspanni@gmail.com>
2021-12-25 15:21:04 +0800

    增加base64 sha1等哈希工具

--- projects/sfx-web/src/pages/utils/md5.tsx
+++ projects/sfx-web/src/pages/utils/md5.tsx
@@ -14,9 +14,33 @@ export default function MD5Page () {
         <button className={'fx-button'}
                 onClick={() => {
                   const result = window.Module.tryCalcMd5(content)
-                  console.debug('content', content, result)
                   setResult(result)
-                }}>计算</button>
+                }}>MD5</button>
+        <button className={'fx-button'}
+                onClick={() => {
+                  const result = window.Module.tryCalcBase64(content)
+                  setResult(result)
+                }}>Base64</button>
+        <button className={'fx-button'}
+                onClick={() => {
+                  const result = window.Module.tryCalcHex(content)
+                  setResult(result)
+                }}>HEX</button>
+        <button className={'fx-button'}
+                onClick={() => {
+                  const result = window.Module.tryCalcSha1(content)
+                  setResult(result)
+                }}>SHA1</button>
+        <button className={'fx-button'}
+                onClick={() => {
+                  const result = window.Module.tryCalcSha256(content)
+                  setResult(result)
+                }}>SHA256</button>
+        <button className={'fx-button'}
+                onClick={() => {
+                  const result = window.Module.tryCalcSha512(content)
+                  setResult(result)
+                }}>SHA512</button>
       </div>
       <div className={'row-result'}>
         {result}
