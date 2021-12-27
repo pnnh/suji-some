3305203a53bd5c15344d51f02f9c2747c03386de
larry <xspanni@gmail.com>
2021-12-27 22:30:27 +0800

    尝试把加载wasm文件的方法从gohtml页面移到前端项目 后续尝试把qt生成的默认实现改为并发分片下载，尽量加快wasm文件下载

--- projects/sfx-web/src/index.tsx
+++ projects/sfx-web/src/index.tsx
@@ -16,6 +16,7 @@ import PersonalPage from '@/pages/account/personal-page'
 import AccountEditPage from '@/pages/account/edit-page'
 import UserInfoPage from '@/pages/user/info-page'
 import TimestampPage from '@/pages/utils/timestamp'
+import { QtLoader, Module } from '@/utils/qtloader'
 
 const App = () => {
   const data = getJsonData<any>()
@@ -51,3 +52,27 @@ const goTopElement = document.getElementById('go-top')
 if (goTopElement) {
   ReactDOM.render(<GoTop/>, goTopElement)
 }
+window.Module = Module
+window.QtLoader = QtLoader
+Module.locateFile = function (path, prefix) {
+  console.log('locateFile', path, prefix)
+  return prefix + path
+}
+document.addEventListener('DOMContentLoaded', function () {
+  console.log('3 seconds passed')
+  const qtCanvas = document.getElementById('qt-canvas')
+  if (qtCanvas) {
+    const qtLoader = window.QtLoader({
+      canvasElements: [qtCanvas],
+      showLoader: function () { },
+      showError: function (errorMessage) {
+        console.log('showError', errorMessage)
+      },
+      showExit: function () {
+      },
+      showCanvas: function () {
+      }
+    })
+    qtLoader.loadEmscriptenModule('http://127.0.0.1:3000/wasm/qt-canvas')
+  }
+})
